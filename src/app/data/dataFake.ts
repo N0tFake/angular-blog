c6fea289c389dfb31923554e31c6ad23674de148
Silvio Otavio <silvio.otavio@mail.uft.edu.br>
2023-10-19 10:04:07 -0300

    feat: Mudanças para um bolg do cuphead

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,32 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Cuphead e Mugman derrotam o Diabo e salvam suas almas.",
+    "description": "Os dois irmãos, que apostaram suas almas em um jogo de dados com o Diabo, conseguiram vencer todos os seus devedores e coletar seus contratos de alma. Eles então enfrentaram o próprio Diabo em seu cassino infernal e o derrotaram em uma batalha épica. Os irmãos devolveram os contratos ao Diabo e rasgaram o seu próprio, libertando-se de sua dívida. Eles voltaram para a Ilha Tintura e foram recebidos como heróis pelos moradores.",
+    "photoCover":"../../../assets/news/diabo.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "O Rei Dado é preso por fraude e trapaça.",
+    "description": "O Rei Dado, o braço direito do Diabo e o dono do cassino onde Cuphead e Mugman fizeram sua aposta fatal, foi preso pela polícia após uma investigação revelar que ele usava dados viciados, cartas marcadas e máquinas caça-níqueis manipuladas para enganar seus clientes. O Rei Dado também foi acusado de extorquir os devedores do Diabo e obrigá-los a trabalhar para ele como seus capangas. O Rei Dado foi levado para a prisão e seu cassino foi fechado.",
+    "photoCover":"../../../assets/news/rei dado.jpg"
+  },
+  {
+    "id":"3",
+    "title": "A Fada do Açúcar revela seus segredos de culinária.",
+    "description": " A Fada do Açúcar, uma das aliadas de Cuphead e Mugman em sua jornada, concedeu uma entrevista exclusiva para a nossa revista e revelou seus segredos de culinária. Ela contou que usa ingredientes mágicos para criar doces deliciosos e encantados, como bombons explosivos, balas saltitantes e pirulitos voadores. Ela também ensinou algumas receitas simples e fáceis para os leitores que querem se aventurar na cozinha mágica.",
+    "photoCover":"../../../assets/news/fada do acucar.jpg"
+  },
+  {
+    "id":"4",
+    "title": "O Barão do Zeppelin se aposenta e doa sua fortuna para a caridade.",
+    "description": "O Barão do Zeppelin, um dos devedores do Diabo que foi derrotado por Cuphead e Mugman, anunciou sua aposentadoria e sua decisão de doar toda a sua fortuna para a caridade. Ele disse que se arrependeu de seus atos maléficos e que quer ajudar os mais necessitados. Ele também pediu desculpas aos irmãos por ter tentado matá-los e disse que espera que eles possam perdoá-lo.",
+    "photoCover":"../../../assets/news/barao zeplin.jpg"
+  },
+  {
+    "id":"5",
+    "title": "A Ilha Tinteiro recebe uma nova atração turística: o Museu do Cuphead.",
+    "description": "A Ilha Tintura, o lar de Cuphead e Mugman, recebeu uma nova atração turística: o Museu do Cuphead, um lugar dedicado à história e às aventuras dos dois heróis. O museu conta com várias salas temáticas, onde os visitantes podem ver objetos, fotos, vídeos e até mesmo interagir com alguns dos personagens que Cuphead e Mugman encontraram em sua jornada. O museu também tem uma loja de souvenirs, onde os fãs podem comprar camisetas, canecas, bonés e outros itens relacionados ao jogo.",
+    "photoCover":"../../../assets/news/ilha-tinteiro.jpg"
   }
 ]
